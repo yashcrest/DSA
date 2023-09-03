b2fdb463d7b96a622aea94de8fa52d3848cd327e
yashcrest <yashcrest@gmail.com>
2023-09-03 15:33:55 +0930

    recursion

---
+++ quickSort.js
@@ -0,0 +1,10 @@
+function divideAndConquer(s){
+    //base case
+    if(s % 2 === 0){
+        return
+    }
+    //recursive case 
+    else {
+        
+    }
+}
--- sumofN.js
+++ sumofN.js
@@ -1,11 +1,48 @@
-let sum = 0
-function sumofN(num, sum){
-    if(num <= 0){
-        return
-    }
-    console.log(sum);
-    sum = sum + num
-    sumofN(num-1, sum)
+// let sum = 0
+// function sumofN(num, sum){
+//     if(num <= 0){
+//         return
+//     }
+//     sum = sum + num
+//     console.log(sum);
+//     sumofN(num-1, sum)
+// }
+
+// sumofN(5, sum);
+
+//sum of all elements of array
+// function sumOfArr (arr){
+//     // base case
+//     if(arr.length === 0) return 0;
+
+//     //recursion case
+//     return arr[0] + sumOfArr(arr.slice(1));
+
+// }
+
+// console.log(sumOfArr([1,2,3,4,5]))
+
+
+//counts total number of elements in array
+// function totalElements(arr){
+//     //base case
+//     if (arr.length === 0) {
+//         return 0;
+//     }
+
+//     //recursive case
+//     return 1 + totalElements(arr.slice(1));
+// }
+// console.log(totalElements([1,2,3,4]))
+
+//max number in array
+function maxNumber (arr) {
+    let maxNum = null
+    // base case
+    if (arr.length === 0){
+        return null
+    } 
+    return Math.max(...arr)
 }
 
-sumofN(5, sum);
+console.log(maxNumber([1,2]))
