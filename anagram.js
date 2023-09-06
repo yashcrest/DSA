cd333a450d5ef448cbfd8a407242adc0aac5e469
yashcrest <yashcrest@gmail.com>
2023-09-06 17:37:56 +0930

    anagram

--- anagram.js
+++ anagram.js
@@ -1,5 +1,11 @@
 function anagram(s,t) {
-    return [s.split('').sort().join(''), s.split('').sort().join('')];
+    let sfreq = {}
+    let tfreq = {}
+
+    for(let i=0; i < s.length; i++){
+        sfreq[i] = (sfreq[i] || 0) + 1
+        tfreq[i] = (tfreq[i] || 0) + 1
+    }
 }
 
 console.log(anagram('rat', 'tar'));
