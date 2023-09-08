e37df936a8c90e856e5232415d173eb15e953707
yashcrest <yashcrest@gmail.com>
2023-09-08 19:27:59 +0930

    top k frequent

--- topKfrequent.js
+++ topKfrequent.js
@@ -1,7 +1,16 @@
-let nums = [1,1,1,1,2,2,3]
+let nums = [2,2,2,2,2,3,1,1,1,1,4,4,4,4,4,4]
 var topKFrequent = function (nums,k){
     let map = new Map()
+    let result = []
     nums.forEach(num => map.set(num, map.get(num) + 1 || 1))
-    return map
+
+    //creating array with maps key/value
+    for (let [key,value] of map){
+        result.push([key,value])
+    }
+
+    //sorting the array
+    let sorted = result.sort((a,b) => b[1] - a[1]); //sorting the result array with highest frequest item
+    return sorted.slice(0,k).map(item => item[0]) // slicing the arrays with the given value of 'k' and using map function to get only the first element of the inside array.
 }
-console.log(topKFrequent(nums, 2))
+console.log(topKFrequent(nums, 1))
