ac71439c4482d5cafbe7af4124b8e7f78668e494
yashcrest <yashcrest@gmail.com>
2023-09-13 15:44:53 +0930

    group anagram

--- groupAnagram.js
+++ groupAnagram.js
@@ -1,13 +1,35 @@
-function groupAnagram(strs) {
-  if (strs.length < 2) {
-    return strs;
+// function groupAnagram(strs) {
+//   //base case
+//   if (strs.length < 2) {
+//     return strs;
+//   }
+
+//   let map = new Map();
+//   for (let s of strs) {
+//     let sorted = s.split("").sort().join("");
+//     if (map.has(sorted)) {
+//       map.set(sorted, [...map.get(sorted), s]);
+//     } else {
+//       map.set(sorted, [s]);
+//     }
+//     return Array.from(map.values());
+//   }
+// }
+
+//top k frequent elements
+
+function topKFrequentElements(nums, k) {
+  //base case
+  if (nums.length < 2) {
+    return nums;
   }
-  for (let i = 0; i < strs.length; i++) {
-    for (let j = i + 1; j < strs.length; j++) {
-      if (
-        strs[i].split("").sort().join("") === strs[j].split("").sort().join("")
-      ) {
-      }
+  let map = new Map();
+  //frequency counter
+  for (let num of nums) {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else {
+      map.set(num, 1);
     }
   }
 }
