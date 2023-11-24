679a67af930a62106127d52a052353322e15857b
yashcrest <yashcrest@gmail.com>
2023-11-24 21:39:23 +1030

    group anagram python code

--- groupAnagram.js
+++ groupAnagram.js
@@ -6,12 +6,16 @@ function groupAnagram(strs) {
 
   let map = new Map();
   for (let s of strs) {
-    let sorted = s.split("").sort().join("");
+    let sorted = s.split("").sort().join(""); // i am basically changing all the strings into the same string.
     if (map.has(sorted)) {
-      map.set(sorted, [...map.get(sorted), s]);
+      map.set(sorted, [...map.get(sorted), s]); // im setting the key as the sorted string and value as the current string (s) itself.
     } else {
       map.set(sorted, [s]);
     }
-    return Array.from(map.values());
   }
+  return Array.from(map.values());
 }
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+console.log(groupAnagram(strs));
