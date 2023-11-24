be70a23b3724960fc103f2fd722e7659f5b54de9
yashcrest <yashcrest@gmail.com>
2023-11-24 20:18:31 +1030

    gropAnagram

--- groupAnagram.js
+++ groupAnagram.js
@@ -1,35 +1,17 @@
-// function groupAnagram(strs) {
-//   //base case
-//   if (strs.length < 2) {
-//     return strs;
-//   }
-
-//   let map = new Map();
-//   for (let s of strs) {
-//     let sorted = s.split("").sort().join("");
-//     if (map.has(sorted)) {
-//       map.set(sorted, [...map.get(sorted), s]);
-//     } else {
-//       map.set(sorted, [s]);
-//     }
-//     return Array.from(map.values());
-//   }
-// }
-
-//top k frequent elements
-
-function topKFrequentElements(nums, k) {
+function groupAnagram(strs) {
   //base case
-  if (nums.length < 2) {
-    return nums;
+  if (strs.length < 2) {
+    return strs;
   }
+
   let map = new Map();
-  //frequency counter
-  for (let num of nums) {
-    if (map.has(num)) {
-      map.set(num, map.get(num) + 1);
+  for (let s of strs) {
+    let sorted = s.split("").sort().join("");
+    if (map.has(sorted)) {
+      map.set(sorted, [...map.get(sorted), s]);
     } else {
-      map.set(num, 1);
+      map.set(sorted, [s]);
     }
+    return Array.from(map.values());
   }
 }
--- topKfrequent.js
+++ topKfrequent.js
@@ -1,16 +1,33 @@
-let nums = [2,2,2,2,2,3,1,1,1,1,4,4,4,4,4,4]
-var topKFrequent = function (nums,k){
-    let map = new Map()
-    let result = []
-    nums.forEach(num => map.set(num, map.get(num) + 1 || 1))
+let nums = [2, 2, 2, 2, 2, 3, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4];
+var topKFrequent = function (nums, k) {
+  let map = new Map();
+  let result = [];
+  nums.forEach((num) => map.set(num, map.get(num) + 1 || 1));
 
-    //creating array with maps key/value
-    for (let [key,value] of map){
-        result.push([key,value])
+  //creating array with maps key/value
+  for (let [key, value] of map) {
+    result.push([key, value]);
+  }
+
+  //sorting the array
+  let sorted = result.sort((a, b) => b[1] - a[1]); //sorting the result array with highest frequest item
+  return sorted.slice(0, k).map((item) => item[0]); // slicing the arrays with the given value of 'k' and using map function to get only the first element of the inside array.
+};
+console.log(topKFrequent(nums, 1));
+
+//top k frequent elements
+function topKFrequentElements(nums, k) {
+  //base case
+  if (nums.length < 2) {
+    return nums;
+  }
+  let map = new Map();
+  //frequency counter
+  for (let num of nums) {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+    } else {
+      map.set(num, 1);
     }
-
-    //sorting the array
-    let sorted = result.sort((a,b) => b[1] - a[1]); //sorting the result array with highest frequest item
-    return sorted.slice(0,k).map(item => item[0]) // slicing the arrays with the given value of 'k' and using map function to get only the first element of the inside array.
+  }
 }
-console.log(topKFrequent(nums, 1))
