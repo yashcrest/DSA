809f25ec57a8c35faaef14973a7f7e7138d8485c
yashcrest <yashcrest@gmail.com>
2023-11-27 16:28:04 +1030

    top k frequent items alternative solution.

--- topKfrequent.js
+++ topKfrequent.js
@@ -1,9 +1,8 @@
-let nums = [2, 2, 2, 2, 2, 3, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4];
 var topKFrequent = function (nums, k) {
   let map = new Map();
   let result = [];
   nums.forEach((num) => map.set(num, map.get(num) + 1 || 1));
-
+  
   //creating array with maps key/value
   for (let [key, value] of map) {
     result.push([key, value]);
@@ -13,9 +12,8 @@ var topKFrequent = function (nums, k) {
   let sorted = result.sort((a, b) => b[1] - a[1]); //sorting the result array with highest frequest item
   return sorted.slice(0, k).map((item) => item[0]); // slicing the arrays with the given value of 'k' and using map function to get only the first element of the inside array.
 };
-console.log(topKFrequent(nums, 1));
 
-//top k frequent elements
+//alternative solution
 function topKFrequentElements(nums, k) {
   //base case
   if (nums.length < 2) {
@@ -30,4 +28,11 @@ function topKFrequentElements(nums, k) {
       map.set(num, 1);
     }
   }
+
+  let sorted = Array.from(map).sort((a,b) => b[1] - a[1]);
+  return sorted.slice(0,k).map(item => item[0]);
 }
+
+
+let nums = [2, 2, 2, 2, 2, 3,3, 1, 1, 1, 1, 4, 4, 4, 4, 5,5,5,5,5];
+console.log(topKFrequent(nums, 2));
