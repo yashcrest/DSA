40e12a58842bb0ce12d7e193a12abcbd22e7b7e3
yashcrest <yashcrest@gmail.com>
2023-12-15 21:02:25 +1030

    2sum II

--- twoSum2.js
+++ twoSum2.js
@@ -1,3 +1,20 @@
-var twoSum = function (numbers, target) {
-    
+let numbers = [2,7,11,15]
+let target = 9
+
+var twoSum = function (nums, target) {
+    let lp = 0
+    let rp = nums.length -1 
+    while(lp < rp) {
+        let sum = nums[lp] + nums[rp]
+        if(sum === target) {
+            return [lp + 1, rp + 1];
+        } else if (sum < target) {
+            lp ++ ;
+        } else {
+            rp -- ; 
+        }
+    }
 }
+
+
+console.log(twoSum(numbers, target));
