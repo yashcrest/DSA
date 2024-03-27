b1625a4b9794f6171959d961cba284246e6c6744
yash shrestha <yashcrest@gmail.com>
2024-03-27 16:43:46 +1100

    adding / commenting base case

--- 3Sum.js
+++ 3Sum.js
@@ -1,35 +1,33 @@
 var threeSum = function (nums) {
-    // first we need to sort this array
-    nums.sort((a,b) => a-b);
-    let res = []
+  // first we need to sort this array
+  nums.sort((a, b) => a - b);
+  let res = [];
 
-    for(let i=0; i< nums.length -2; i++ ) {
-        if(i > 0 && nums[i] === nums[i - 1] ) continue;
-        
-        // we can use the same technique we used in 2sum II i.e. 2pointers technique
-        let lp = i + 1;
-        let rp = nums.length - 1
-        while(lp < rp) {
-            let sum = nums[lp] + nums[rp] + nums[i]
-            if(sum === 0) {
-                res.push([nums[i], nums[lp], nums[rp]]);
-                // while on same iteration, check if there is other possible combination
-                while(nums[lp] === nums[lp + 1]) lp ++;
-                while(nums[rp] === nums[rp -1]) rp --;
-                lp ++;
-                rp --;
-            }
-            else if(sum < 0) {
-                lp ++;
-            } else {
-                rp -- ;
-            }
-        }
+  for (let i = 0; i < nums.length - 2; i++) {
+    //base case; this is ran everytime and will exit curr iteration if the succeeding number is the same
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    // we can use the same technique we used in 2sum II i.e. 2pointers technique
+    let lp = i + 1;
+    let rp = nums.length - 1;
+    while (lp < rp) {
+      let sum = nums[lp] + nums[rp] + nums[i];
+      if (sum === 0) {
+        res.push([nums[i], nums[lp], nums[rp]]);
+        // while on same iteration, check if there is other possible combination
+        while (nums[lp] === nums[lp + 1]) lp++;
+        while (nums[rp] === nums[rp - 1]) rp--;
+        lp++;
+        rp--;
+      } else if (sum < 0) {
+        lp++;
+      } else {
+        rp--;
+      }
     }
-    return res
-}
+  }
+  return res;
+};
 
-
-
-let nums = [-1,0,1,2,-1,-4];
+let nums = [-1, 0, 1, 2, -1, -4];
 console.log(threeSum(nums));
